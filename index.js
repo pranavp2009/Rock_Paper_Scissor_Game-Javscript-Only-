2b1820b2c9a5b515d9892e35c079d70f2f63c791
pranavp2009 <pranavp0209@gmail.com>
2026-03-28 10:34:30 +0300

    Removed Unnecessary Comments

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-/*
-The logic
-    function:
-        getComputerChoice() -> It gets the computers choice
-            Random logic -> Math.floor(Math.random() * (3)) + 1
-        getHumanChoice() -> It gets the humans choice
-        playRound() -> It plays one round
-        playGame() -> It plays the game (5 rounds)
-*/
-
 function getComputerChoice(){
     let compChoice = Math.floor(Math.random() * (3)) + 1
 
